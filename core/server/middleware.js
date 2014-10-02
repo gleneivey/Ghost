ae412c17bef673d16b35eac96e91697aac96dfd8
Glen E. Ivey <gei@mcn.org>
2014-10-02 06:56:11 -0700

    When used as middleware, output a log message on startup *after* the initialization promise resolves

--- core/server/middleware.js
+++ core/server/middleware.js
@@ -4,6 +4,13 @@ var _      = require('lodash'),
     config = require('./config'),
     ghost  = require('./index');
 
+function logStartMessages() {
+    console.log('Ghost middleware configured and ready to serve requests'.green);
+    if (process.env.NODE_ENV !== 'production') {
+        console.log(('Ghost is running in environment "' + process.env.NODE_ENV + '"...').grey);
+    }
+}
+
 function buildServer(configValues) {
     var setupResults, message,
         promise = config.init(_.merge(configValues, {asMiddleware: true}), 'middleware'),
@@ -19,6 +26,8 @@ function buildServer(configValues) {
     ghostPromise = setupResults[0];
     middlewareInstance = setupResults[1];
     middlewareInstance.ghostPromise = ghostPromise;
+
+    ghostPromise.then(logStartMessages);
 
     return middlewareInstance;
 }
